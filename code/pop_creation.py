a703c514c4ef9ad7fbb293d3b822ad8605ebba4e
Loris Cousin <loris.cousin@student.isae-supaero.fr>
2025-05-14 16:32:45 +0200

    population lichen

---
+++ code/pop_creation.py
@@ -0,0 +1,39 @@
+from samples_set import SamplesSet
+import numpy as np
+
+if __name__ == "__main__":
+    # Créer un ensemble de samples
+
+    # #POP 1
+    # x_1 = 12040
+    # y_1 = 15730
+    
+    # row_start = int(np.round(x_1/32))*32 #row X
+    # column_start = int(np.round(y_1/32))*32 #column Y
+    # n_samples_x = 20
+    # n_samples_y = 20
+    
+    # size_patch= 32
+    # pop_lichen_1 = SamplesSet(n_samples_x=n_samples_x, n_samples_y=n_samples_y, category="lichen")
+    # pop_lichen_1.create_samples_grid(x_start=row_start, y_start=column_start, size_patch=size_patch)
+    # pop_lichen_1.plot_samples()
+
+    # path = "data/samples/lichen/"
+    # pop_lichen_1.save_samples_to_json(filename=path+"pop_lichen_1.json")
+
+    #POP 2
+    x_2 = 19300
+    y_2 = 8520
+    
+    row_start = int(np.round(x_2/32))*32 #row X
+    column_start = int(np.round(y_2/32))*32 #column Y
+    n_samples_x = 20
+    n_samples_y = 20
+    size_patch= 32
+    pop_lichen_2 = SamplesSet(n_samples_x=n_samples_x, n_samples_y=n_samples_y, category="lichen")
+    pop_lichen_2.create_samples_grid(x_start=row_start, y_start=column_start, size_patch=size_patch)
+    pop_lichen_2.plot_samples()
+    path = "data/samples/lichen/"
+    pop_lichen_2.save_samples_to_json(filename=path+"pop_lichen_2.json")
+
+
