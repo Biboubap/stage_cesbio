b8ab2723421f2e0d092c78cf82b905a496102e5b
Loris Cousin <loris.cousin@student.isae-supaero.fr>
2025-06-02 16:49:40 +0200

    wap23

--- code/Samples2/tiles_gdal.sh
+++ code/Samples2/tiles_gdal.sh
@@ -3,3 +3,8 @@ gdal_retile.py -targetDir drone_treated/WAP32_tiles/rgb -ps 4992 4992 -of GTiff 
 gdal_retile.py -targetDir drone_treated/WAP32_tiles/dsm -ps 4992 4992 -of GTiff drone_treated/WAP32_full_dsm.tif 
 
 gdalwarp drone_treated/WAP32_tiles/classification/*.tif drone_treated/WAP32_tiles/WAP32_classif.tif 
+
+
+
+gdal_retile.py -targetDir drone_treated/WAP23_tiles/rgb -ps 4992 4992 -of GTiff drone_treated/Wap23_main_transparent_mosaic_group1.tif 
+gdal_retile.py -targetDir drone_treated/WAP23_tiles/dsm -ps 4992 4992 -of GTiff drone_treated/Wap23_main_dsm.tif 
